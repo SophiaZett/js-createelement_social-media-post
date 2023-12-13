69a78e475d773514b4f9c9c377ec74f360c421b3
Sophia S. <sophia0791@icloud.com>
2023-12-13 12:41:09 +0100

    session solution

--- js/index.js
+++ js/index.js
@@ -10,3 +10,28 @@ likeButton.addEventListener("click", handleLikeButtonClick);
 
 // Exercise:
 // Use document.createElement() and append another social media post to the body.
+const newPost = document.createElement("article");
+newPost.classList.add("post");
+
+const postText = document.createElement("p");
+postText.classList.add("post__content");
+postText.textContent ="Das bootcamp is so unfassbar krass schwer für mich!!!";
+
+const postFooter = document.createElement("footer");
+postFooter.classList.add("post__footer");
+
+const postUser =document.createElement("span");
+postUser.classList.add("post__username");
+postUser.textContent = "@Linetti";
+
+const postButton =document.createElement("button");
+postButton.classList.add("post__button");
+postButton.textContent = "♥ Like";
+
+newPost.append(postText, postFooter);
+postFooter.append(postUser, postButton);
+document.body.append(newPost);
+
+postButton.addEventListener("click", handleLikeButtonClick);
+
+console.log(newPost);
